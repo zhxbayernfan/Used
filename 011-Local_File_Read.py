ffbbc591daad332e98df3753d3d30ed65399efa5
zhxbayernfan <1303425363@qq.com>
2019-09-06 18:50:37 +0800

    renewal_old_files_and_add_study_new_today

---
+++ 011-Local_File_Read.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3 
+f = open('E:/Python/s190904.txt', 'r')
+for line in f:
+    print(line, end='')
+f.close()
+print ("1")
+File = open('190904.txt', 'r')
+for line in File:
+    print(line, end='')
+File.close()
+print ("2")
+File2 = open('E:/Python/s190801.sh', 'r')
+for line in File2:
+    print(line, end='')
+File2.close()
+print ("3")

61a0b6b8391955c32aa8911a42084b9b9d138184
zhxbayernfan <1303425363@qq.com>
2019-11-01 08:41:26 +0800

    edit191101_morning3

---
+++ 025-small_panel_infocut_vision191101_in_Used.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3 
+
+import csv
+
+File1 = open('E:/Python/025/191027_86gene_180518.csv', 'r')
+for line in File1:
+    print(line, end='')
+File1.close()
+print ("1")
+
+#本代码的后续更新请移步Hap仓库

0107df4b41ebd5f984c55814c84e927e5b4d2c0b
zhxbayernfan <1303425363@qq.com>
2019-12-18 20:18:34 +0800

    Shellscript191218_ssecret

---
+++ 028-1-zcat_191218_ss.sh
@@ -0,0 +1,15 @@
+zcat s068_SZ20'*'190330013PET-e_ffpe'*'dna_wEspluS_22660_S53_L002_R1_001.fastq.gz s071_SZ20'*'190330013PET-e_ffpe'*'dna_wEspluS_22171_S21_L004_R1_001.fastq.gz >merge_22171_R1.fastq && gzip merge_22171_R1.fastq
+#finished! 3_x02
+zcat s068_SZ20'*'190330013PET-e_ffpe'*'dna_wEspluS_22660_S53_L002_R2_001.fastq.gz s071_SZ20'*'190330013PET-e_ffpe'*'dna_wEspluS_22171_S21_L004_R2_001.fastq.gz >merge_22171_R2.fastq && gzip merge_22171_R2.fastq
+#doing... 3_x02
+zcat s092_SZ20'*'190326026WHB-b_g'*'dna_wEspluS_21812_S15_L004_R1_001.fastq.gz s009_SZ20'*'190326026WHB-b_g'*'dna_wEspluS_24629_S3_L003_R1_001.fastq.gz >merge_24629_R1.fastq && gzip merge_24629_R1.fastq
+#finished! 4_x02
+zcat s092_SZ20'*'190326026WHB-b_g'*'dna_wEspluS_21812_S15_L004_R2_001.fastq.gz s009_SZ20'*'190326026WHB-b_g'*'dna_wEspluS_24629_S3_L003_R2_001.fastq.gz >merge_24629_R2.fastq && gzip merge_24629_R2.fastq
+#doing... 4_x02
+zcat s077_SZ20'*'190423018FPT-1_tt'*'dna_wEspluS_24269_S12_L004_R1_001.fastq.gz s014_SZ20'*'190423018FPT-1_tt'*'dna_wEspluS_24634_S8_L003_R1_001.fastq.gz >merge_24634_R1.fastq && gzip merge_24634_R1.fastq
+#finished! 5_x02
+zcat s077_SZ20'*'190423018FPT-1_tt'*'dna_wEspluS_24269_S12_L004_R2_001.fastq.gz s014_SZ20'*'190423018FPT-1_tt'*'dna_wEspluS_24634_S8_L003_R2_001.fastq.gz >merge_24634_R2.fastq && gzip merge_24634_R2.fastq
+#doing.. 5_x02
+#6_x02 before201903 downloading...
+#7_x02 after201904_R1 downloading...
+#8_x02 after201904_R2 downloading...

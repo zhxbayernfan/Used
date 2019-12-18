9097685eed67d9b630d6f899a76cc001465c0a4c
zhxbayernfan <1303425363@qq.com>
2019-12-18 19:44:19 +0800

    ShellScript191218_2

--- 028-1-zcat_191218.sh
+++ 028-1-zcat_191218.sh
@@ -1,12 +1,15 @@
 zcat S068_SZ20190330013PET-e_ffpedna_wesplus_22660_S53_L002_R1_001.fastq.gz S071_SZ20190330013PET-e_ffpedna_wesplus_22171_S21_L004_R1_001.fastq.gz >merge_22171_R1.fastq && gzip merge_22171_R1.fastq
-#finished 3_x02
+#finished! 3_x02
 zcat S068_SZ20190330013PET-e_ffpedna_wesplus_22660_S53_L002_R2_001.fastq.gz S071_SZ20190330013PET-e_ffpedna_wesplus_22171_S21_L004_R2_001.fastq.gz >merge_22171_R2.fastq && gzip merge_22171_R2.fastq
-#doing 3_x02
+#doing... 3_x02
 zcat S092_SZ20190326026WHB-b_gdna_wesplus_21812_S15_L004_R1_001.fastq.gz S009_SZ20190326026WHB-b_gdna_wesplus_24629_S3_L003_R1_001.fastq.gz >merge_24629_R1.fastq && gzip merge_24629_R1.fastq
-#finished 4_x02
+#finished! 4_x02
 zcat S092_SZ20190326026WHB-b_gdna_wesplus_21812_S15_L004_R2_001.fastq.gz S009_SZ20190326026WHB-b_gdna_wesplus_24629_S3_L003_R2_001.fastq.gz >merge_24629_R2.fastq && gzip merge_24629_R2.fastq
-#doing 4_x02
+#doing... 4_x02
 zcat S077_SZ20190423018FPT-1_ttdna_wesplus_24269_S12_L004_R1_001.fastq.gz S014_SZ20190423018FPT-1_ttdna_wesplus_24634_S8_L003_R1_001.fastq.gz >merge_24634_R1.fastq && gzip merge_24634_R1.fastq
-#finished 5_x02
+#finished! 5_x02
 zcat S077_SZ20190423018FPT-1_ttdna_wesplus_24269_S12_L004_R2_001.fastq.gz S014_SZ20190423018FPT-1_ttdna_wesplus_24634_S8_L003_R2_001.fastq.gz >merge_24634_R2.fastq && gzip merge_24634_R2.fastq
-#doing 5_x02
+#doing.. 5_x02
+#6_x02 before201903 downloading...
+#7_x02 after201904_R1 downloading...
+#8_x02 after201904_R2 downloading...
